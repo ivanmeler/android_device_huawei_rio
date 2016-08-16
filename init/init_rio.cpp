e061d16a0b0886aa31efe9f0fad761d706cac5ba
LuK1337 <priv.luk@gmail.com>
2016-08-17 00:55:11 +0200

    init: Set ro.alarm_boot prop

--- init/init_rio.cpp
+++ init/init_rio.cpp
@@ -34,6 +34,37 @@
 #include "log.h"
 #include "util.h"
 
+static void init_alarm_boot_properties()
+{
+    int boot_reason;
+    FILE *fp;
+
+    fp = fopen("/proc/sys/kernel/boot_reason", "r");
+    fscanf(fp, "%d", &boot_reason);
+    pclose(fp);
+
+    /*
+     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+     * For existing PMIC chips, the following mapping applies
+     * for the value of boot_reason:
+     *
+     * 0 -> unknown
+     * 1 -> hard reset
+     * 2 -> sudden momentary power loss (SMPL)
+     * 3 -> real time clock (RTC)
+     * 4 -> DC charger inserted
+     * 5 -> USB charger inserted
+     * 6 -> PON1 pin toggled (for secondary PMICs)
+     * 7 -> CBLPWR_N pin toggled (for external power supply)
+     * 8 -> KPDPWR_N pin toggled (power key pressed)
+     */
+     if (boot_reason == 3) {
+        property_set("ro.alarm_boot", "true");
+     } else {
+        property_set("ro.alarm_boot", "false");
+     }
+}
+
 void vendor_load_properties()
 {
     std::string platform;
@@ -44,6 +75,8 @@ void vendor_load_properties()
     platform = property_get("ro.board.platform");
     if (platform == ANDROID_TARGET)
         return;
+
+    init_alarm_boot_properties();
 
     fin.open("/proc/app_info");
     while (getline(fin, buf))
