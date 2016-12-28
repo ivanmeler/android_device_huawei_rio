49d079ae3632b847bbe27fcf1ec29cfb0f7a7c4c
Surdu Petru <surdu_petru@yahoo.com>
2016-12-28 13:00:20 +0100

    add 2016 CM Copyright

--- ril/telephony/java/com/android/internal/telephony/HwQualcommRIL.java
+++ ril/telephony/java/com/android/internal/telephony/HwQualcommRIL.java
@@ -3,6 +3,7 @@
  * Not a Contribution.
  *
  * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
