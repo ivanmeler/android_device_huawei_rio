5434ebe8053911b9a09d37492da9fa3b0e43ab94
Ivan Meler <i_ivan@windowslive.com>
2017-11-07 12:06:20 +0100

    Underp building hidl packages

--- device.mk
+++ device.mk
@@ -280,6 +280,7 @@ PRODUCT_PACKAGES += \
     wcnss_service
 
 # Hidl hals
+PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
